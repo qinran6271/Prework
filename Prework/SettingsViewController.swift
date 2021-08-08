eeead2ae5f5294449953e778c6b1f55c5e4f2694
qinran6271 <60624760+qinran6271@users.noreply.github.com>
2021-08-08 19:32:50 -0400

    add Seetings

---
+++ Prework/SettingsViewController.swift
@@ -0,0 +1,64 @@
+//
+//  SettingsViewController.swift
+//  Prework
+//
+//  Created by Qinran Wang on 8/5/21.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    @IBOutlet weak var text: UILabel!
+    @IBOutlet weak var text1: UILabel!
+    @IBOutlet weak var text2: UILabel!
+    @IBOutlet weak var text3: UILabel!
+    @IBOutlet weak var text4: UILabel!
+    
+    @IBOutlet weak var tip1: UITextField!
+    
+    @IBOutlet weak var dayNightSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func tip1Change(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        let tip1Changed = Double(tip1.text!) ?? 0
+        defaults.set(tip1Changed, forKey: "tip1")
+        defaults.synchronize()
+    }
+    
+    @IBAction func switchDidChange(_ sender:UISwitch ) {
+        let defaults = UserDefaults.standard
+        if sender.isOn{
+            view.backgroundColor = .black
+            text.textColor = .white
+            text1.textColor = .white
+            text2.textColor = .white
+            text3.textColor = .white
+            text4.textColor = .white
+        }
+        else {
+            
+            view.backgroundColor = .white
+            text.textColor = .black
+            defaults.set(sender.isOn, forKey: "Switch")
+        }
+        defaults.synchronize()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -8,33 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var billAmountTextField: UITextField!
     @IBOutlet weak var tipAmountLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var totalLabel: UILabel!
 
+    @IBOutlet weak var partySizeControl: UIStepper!
+    @IBOutlet weak var partySize: UILabel!
+    
+    @IBOutlet weak var perPersonCost: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // Sets the title in the Navigation Bar
+        self.title = "Tip Calculator"
+        
     }
 
     @IBAction func calculateTip(_ sender: Any) {
+        let defaults = UserDefaults.standard
         // Get bill amount for text field input
         let bill = Double(billAmountTextField.text!) ?? 0
+        let tip_1 = defaults.double(forKey: "tip1")
+        let partySizeChange = defaults.double(forKey: "partySizeChange")
         
         // Get Total tip by multiplying tip * tipPercentages
         let tipPercentages = [0.15, 0.18, 0.2]
         let tip = bill *
             tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
+        defaults.set(total, forKey: "total")
+        
+        let perPerson = total/partySizeChange
         
         // Update Tip Amount Label
         tipAmountLabel.text = String(format:"$%.2f", tip)
         //Update Total Amount
         totalLabel.text = String(format:"$%.2f", total)
+        perPersonCost.text = String(format:"$%.2f",perPerson)
+        defaults.synchronize()
+        
     }
     
+    
+    @IBAction func partySizeChange(_ sender: UIStepper) {
+        let defaults = UserDefaults.standard
+        partySize.text = String(Int(sender.value))
+        defaults.set(sender.value, forKey: "partySizeChange")
+        let total = defaults.double(forKey: "total")
+        let perPerson = total/sender.value
+        perPersonCost.text = String(format:"$%.2f",perPerson)
+        defaults.synchronize()
+    }
     
 }
 
